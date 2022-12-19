c64ab63b5b7bd4d40ad289230ef368ef789895b2
Agoston Herczegh <82599483+AgostonHerczegh@users.noreply.github.com>
2022-12-19 14:25:11 +0100

    Main.java updated. Feladatsor 8. feladatáig done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            BufferedReader beolvasas = new BufferedReader(new FileReader("bukkm2019.txt"));
+            List<Versenyzo> participants = new ArrayList<>();
+            beolvasas.readLine();
+            String sor;
+
+
+            while((sor = beolvasas.readLine())!=null){
+                Versenyzo versenyzo = new Versenyzo(sor);
+                participants.add(versenyzo);
+            }
+            for(Versenyzo item : participants) {
+                System.out.println(item.getRajtszam() + " " + item.getKategoria() + " " + item.getNev()
+                        + " " + item.getEgyesulet() + " " + item.getIdo());
+            }
+            }catch(IOException r){
+            r.printStackTrace();
+
+        }
+
+
     }
 }
